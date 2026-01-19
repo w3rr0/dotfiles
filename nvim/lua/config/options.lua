2cd260c3430a09958bf38e41ebcfef96e124fe7e
w3rr0 <konradmateja65@gmail.com>
2026-01-19 23:44:28 +0100

    increase line scrolloff

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,3 +5,4 @@
 vim.opt.shiftwidth = 4 -- indentation size
 vim.opt.tabstop = 4 -- tabs equal 4 spaces
 vim.opt.expandtab = true -- change tabs for spaces
+vim.opt.scrolloff = 10 -- keep 10 lines above and below the cursor
