b4ea27c63b2ae3b526b35ebaddef5434d4d0222f
w3rr0 <konradmateja65@gmail.com>
2026-02-17 16:22:48 +0100

    add hyprland

--- install.sh
+++ install.sh
@@ -10,30 +10,30 @@ BOLD=$(printf '\033[1m')
 NC=$(printf '\033[0m')
 
 link_file() {
-	src="$1"
-	dest="$2"
+    src="$1"
+    dest="$2"
 
-	dest_dir=$(dirname "$dest")
-	if [ ! -d "$dest_dir" ]; then
-		mkdir -p "$dest_dir"
-	fi
+    dest_dir=$(dirname "$dest")
+    if [ ! -d "$dest_dir" ]; then
+        mkdir -p "$dest_dir"
+    fi
 
-	if [ -e "$dest" ] || [ -L "$dest" ]; then
-		
-		if [ -L "$dest" ]; then
-			current_target=$(ls -ld "$dest" | awk '{print $NF}')
-			if [ "$current_target" = "$src" ]; then
-				printf "${GREEN}Already linked: %s${NC}\n" "$dest"
-				return
-			fi
-		fi
+    if [ -e "$dest" ] || [ -L "$dest" ]; then
 
-		printf "${YELLOW}Collision found, creating backup: %s.backup${NC}\n" "$dest"
-		mv "$dest" "$dest.backup"
-	fi
+        if [ -L "$dest" ]; then
+            current_target=$(ls -ld "$dest" | awk '{print $NF}')
+            if [ "$current_target" = "$src" ]; then
+                printf "${GREEN}Already linked: %s${NC}\n" "$dest"
+                return
+            fi
+        fi
 
-	ln -s "$src" "$dest"
-	printf "${GREEN} Symlink created: %s -> %s${NC}\n" "$dest" "$src"
+        printf "${YELLOW}Collision found, creating backup: %s.backup${NC}\n" "$dest"
+        mv "$dest" "$dest.backup"
+    fi
+
+    ln -s "$src" "$dest"
+    printf "${GREEN} Symlink created: %s -> %s${NC}\n" "$dest" "$src"
 }
 
 # Neovim
@@ -47,5 +47,13 @@ link_file "$DOTFILES_DIR/starship/starship.toml" "$CONFIG_DIR/starship.toml"
 
 # Tmux
 link_file "$DOTFILES_DIR/tmux/.tmux.conf" "$HOME/.tmux.conf"
+
+# ---Arch only---
+if [ "$1" == "--arch" ]; then
+
+    # Hyprland
+    link_file "$DOTFILES_DIR/hypr" "$CONFIG_DIR/hypr"
+
+fi
 
 printf "\n${BLUE}${BOLD}All symlinks created${NC}\n"
